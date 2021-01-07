21f229e025b87b55e63378a127cf36c317e59f0c
binary-rainer <rainer.koehler@univie.ac.at>
2021-01-07 19:23:19 +0000

    use results of svdfit for start points

--- python/MCfit.py
+++ python/MCfit.py
@@ -3,7 +3,7 @@
 """
 MCfit.py
 Created:     Tue Nov  3 19:59:55 2020 by Koehler@Rita
-Last change: Wed Nov  4 12:45:38 2020
+Last change: Thu Jan  7 18:39:39 2021
 
 Python-script to do something
 """
@@ -17,8 +17,9 @@ import emcee
 import corner
 
 from datetime import datetime
+from astropy.io  import fits
 from astropy.time import Time
-from read_points import read_points
+from astropy.table import Table
 from KeplerOrbit import KeplerOrbit
 
 #############################################################################
@@ -40,8 +41,13 @@ def log_probability( el, obs, dummy):
     PA_exp = np.arctan2(xexp,yexp) * 180./np.pi
     PA_exp[ PA_exp<0 ] += 360.	# shift into range [0,360]
 
-    dr = (obs["Sep"]-sepexp) / obs["eSep"]
-    dp = (obs["PA"] -PA_exp) / obs["ePA"]
+    # column names from text file:
+    #dr = (obs["Sep"]-sepexp) / obs["eSep"]
+    #dp = (obs["PA"] -PA_exp) / obs["ePA"]
+
+    # column names from fits file (what was I thinking???)
+    dr = (obs["Separation"]    -sepexp) / obs["Sep Error"]
+    dp = (obs["Position Angle"]-PA_exp) / obs["PA Error"]
 
     chisq = np.sum( dr*dr + dp*dp )
 
@@ -50,30 +56,49 @@ def log_probability( el, obs, dummy):
 
 #############################################################################
 
-for f in sys.argv[1:]:
-    print(f)
-    starname, obs = read_points(f)
+nWalker = 42
+nDim = 7	# actually, number of elements
 
-    print(starname)
-    print(obs)
 
-    nWalker = 42
-    nDim = 7
+# this has different column names than inside class OrbitFit!
+obs = Table.read("TTau-SaSb_obs.fits")
+print("obs table:")
+print(obs.info)
 
-    startpos = np.array([55348., 6.031, 68.5, 0.79, 255.3, 36.9, 97.0]) + 0.1 * np.random.randn( nWalker, nDim)
 
-    sampler = emcee.EnsembleSampler( nWalker, nDim, log_probability, args=(obs,0))
-    sampler.run_mcmc(startpos, 1000, progress=True);
+svdres = fits.getdata("SaSb-fit01_fit.fits")
+print("shape of svd results:", svdres.shape)
 
-    samps = sampler.get_chain(flat=True)
-    print(samps.shape)
 
-    plt.plot(samps[:,2], ".")	# axis
-    plt.show()
+fchisq = svdres[7,...].flatten()
 
-    labels = [ "T0", "period", "axis", "e", "omega", "Omega", "i" ]
-    fig = plt.figure()
-    fig = corner.corner(samps, fig=fig, labels=labels)
-    plt.show()
+# nWalker indices for best chi-squares
+bestidx = (np.argsort(fchisq))[0:nWalker]
+
+startpnts = np.zeros( (nWalker, nDim) )
+
+for iEl in range(nDim):
+    print(iEl)
+
+    fparam = svdres[iEl,...].flatten()
+    startpnts[:,iEl] = fparam[bestidx]
+
+
+print(startpnts.shape)
+
+
+sampler = emcee.EnsembleSampler( nWalker, nDim, log_probability, args=(obs,0))
+sampler.run_mcmc(startpnts, 1000, progress=True);
+
+samps = sampler.get_chain(flat=True)
+print(samps.shape)
+
+plt.plot(samps[:,2], ".")	# axis
+plt.show()
+
+labels = [ "T0", "period", "axis", "e", "omega", "Omega", "i" ]
+fig = plt.figure()
+fig = corner.corner(samps, fig=fig, labels=labels)
+plt.show()
 
 #############################################################################
